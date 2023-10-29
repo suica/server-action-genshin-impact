57951d475202d81640ce8d4aa456bb874e2cf965
Sg <suica@users.noreply.github.com>
2023-10-30 00:20:05 +0800

    chore: remove unnecessary words from the config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (/** @type {import('webpack').Configuration} */ config) => {
-    if (Array.isArray(config.externals)) {
-      config.externals.push('@napi-rs/system-shutdown')
-    } else {
-      config.externals = ['@napi-rs/system-shutdown']
-    }
-    return config
-  }
-}
+    return config;
+  },
+};
 
 module.exports = nextConfig
